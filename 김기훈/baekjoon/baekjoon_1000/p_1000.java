34d8f6a6030a049de683587bbdccc8f0814f874e
hoon0912 <kkhoon912@gmail.com>
2021-05-29 01:50:55 +0900

    1주차

---
+++ 김기훈/baekjoon/baekjoon_1000/p_1000.java
@@ -0,0 +1,8 @@
+public class p_1000 {
+    public static void main(String[] args) throws java.io.IOException{
+        int a = System.in.read() - '0';
+        System.in.read();
+        int b = System.in.read() - '0';
+        System.out.println(a+b);
+    }
+}
