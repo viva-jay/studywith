4a7c53f8b179941a28de186c20856b5965be1a92
serena-git <togoliketheblossoms@gmail.com>
2022-01-30 19:58:28 +0900

    delete files

--- 권순규/week01/ex_125.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s = [w.lower() for w in s if w.isalnum()]
-        return s == s[::-1]
--- 권순규/week03/ex_561.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-
-class Solution:
-    def arrayPairSum(self, nums: List[int]) -> int:
-        return sum(sorted(nums)[::2])
-
-print(Solution().arrayPairSum([1,4,3,2]))
-print(Solution().arrayPairSum([6,2,6,5,1,2]))
